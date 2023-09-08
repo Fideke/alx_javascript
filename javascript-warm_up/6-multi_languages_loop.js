a6dd7e90a4516e5b6e29cdf9c23e156b8036d311
Fideke <nadabafidel@gmail.com>
2023-09-08 14:46:17 -0800

    java

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// a string that prints 3 lines but using an array of a string and aloop
+const fide = ["C is fun",
+"Python is cool",
+"JavaScript is amazing"];
+for (let i = 0; i < 3; i++){
+    console.log(fide[i]);
+}
