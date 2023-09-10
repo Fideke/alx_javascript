f4b23dfa002e902d6250793635b522d44672db7b
Fideke <nadabafidel@gmail.com>
2023-09-09 17:24:41 -0800

    JAVA

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,6 +2,7 @@
 // a class rectangle that defines a rectangle
 // use class notation for defining your class
 // constructor must take two arguements
+
 class Rectangle {
     constructor (w, h) {
         if (w > 0 && h > 0) {
