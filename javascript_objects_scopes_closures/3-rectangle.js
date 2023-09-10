bec583f31b0680555cec82694397972f78c5653b
Fideke <nadabafidel@gmail.com>
2023-09-10 11:16:42 -0800

    rectangle

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -6,7 +6,10 @@ class Rectangle {
             this.height = h;
         }
     }
+    print() {
+        for (let i = 0; i < this.height; i++) {
+            console.log("X".repeat(this.width));
+        }
+    }
 }
-Rectangle = x
-print(x)
 module.exports = Rectangle;
