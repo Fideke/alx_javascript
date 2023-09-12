c2ef3f7afeed5d699f472e6b84ca39729a526bfd
Fideke <nadabafidel@gmail.com>
2023-09-12 15:53:29 -0800

    status

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -5,6 +5,9 @@ const request = require('request');
 
 const url = process.argv[2];
 
-request.get(url, (error, response, body) => {
+request(url, (error, response, body) => {
+    if (error) {
+        console.error(error);
+    }
     console.log('code: ', response.statusCode);
 });
