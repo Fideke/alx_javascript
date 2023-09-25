3fb63130986a3eaf37654e2a14aa16a14bf17169
Fideke <nadabafidel@gmail.com>
2023-09-25 16:40:15 +0300

    java

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -5,7 +5,7 @@ const movie_ID = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/:id';
 const mainUrl = url.concat(movie_ID);
 
-request(mainUrl, function(error, response, body) {
+request(mainUrl, (error, response, body) => {
     if (error) {
         console.error(error);
     }
