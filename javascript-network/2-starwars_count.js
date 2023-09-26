728ef26466143c9ace06fbdde23890a3b2516bf4
Fideke <nadabafidel@gmail.com>
2023-09-26 15:29:40 +0300

    fs

---
+++ javascript-network/2-starwars_count.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+const request = require('request');
+const API_URL = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/';
---
+++ javascript-network/3-request_store.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const { error } = require('console');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, Response, body) => {
+    if (error) {
+        console.error(error);
+    }
+    const data = body;
+
+    fs.writeFile(filePath, data, 'utf-8', (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+});
